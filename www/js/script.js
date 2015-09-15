c7f804e8d0ecb3f44840244f8fcd95309b83725e
Xteal <Kevin_Rovira@msn.om>
2015-09-15 22:15:18 +0200

    cambios generales

--- www/js/script.js
+++ www/js/script.js
@@ -1,10 +1,21 @@
 $(function() {
-	$("#bienvenido").on("click",function() {
-		$("#containter").load("bienvenido.html");
+	$(".back-to-menu").on("click", function() {
+		$("#pagina").hide();
+		$(".back-to-menu").hide();
+	});
+	
+	$("nav li>a").on("click",function() {
+		var id=$(this).attr("id");
+		if(id!="undefined") {
+			cargarPagina($(this).attr("id"));
+		}
 	});
 });
 
 function cargarPagina(pagina) {
-	$("#containter").load(pagina+".html #pagina");
+	$("#pagina").load(pagina+".html");
+	$(".back-to-menu").show();
+	$("nav").hide();
 	$("#pagina").show();
+	
 }
